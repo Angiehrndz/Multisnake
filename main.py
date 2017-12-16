90395ccb97b1001d1f32e49602414673d909f64c
Axel Wickman <axelwickm@gmail.com>
2017-12-17 00:54:23 +0100

    Collisions between snakes

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from pygame.locals import *
 PIXELS_PER_GRID = 20
 
 class Snake:
-    def __init__(self, startingCoordinate, startingDirection):
+    def __init__(self, game, startingCoordinate, startingDirection):
+        self.game = game
+
         self.chain = [startingCoordinate]
-        for i in range(3):
+        for i in range(10):
             n = [startingCoordinate[0]-i-1, startingCoordinate[1]]
             self.chain.append(n)
 
         self.direction = startingDirection
-        self.speed = 1000
+        self.speed = 150
 
         self.speedtimer = 0
 
@@ -23,10 +25,14 @@ class Snake:
 
     def update(self):
 
+        # Move snake body
+
         for i in reversed(range(len(self.chain))):
             if i!= 0:
                 self.chain[i][0] = self.chain[i-1][0]
                 self.chain[i][1] = self.chain[i-1][1]
+
+        # Move snake head
 
         if self.direction == 0:
             self.chain[0][0] += 1
@@ -37,6 +43,24 @@ class Snake:
         if self.direction == 3:
             self.chain[0][1] += -1
 
+        # Check head in wall
+
+        if self.chain[0][0] >= self.game.gridSize[0]:
+            self.kill()
+        if self.chain[0][1] >= self.game.gridSize[1]:
+            self.kill()
+        if self.chain[0][0] < 0:
+            self.kill()
+        if self.chain[0][1] < 0:
+            self.kill()
+
+        # Check head in other snake's body
+
+        collided = self.game.snakeCollision(self)
+
+        if type(collided) == Snake:
+            if collided != self:
+                self.kill()
 
     def on_render(self, screen):
         for i in range(len(self.chain)):
@@ -49,6 +73,9 @@ class Snake:
     def setDirection(self, direction):
         if (self.direction - direction)%2 != 0:
             self.direction = direction
+
+    def kill(self):
+        print("NOW I WILL KILL MYSELF! D:")
 
 
 class Game:
@@ -66,8 +93,28 @@ class Game:
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
 
-        self.ownSnake = Snake([10,0], 0)
-        self.otherSnakes = []
+        self.ownSnake = Snake(self, [10,0], 0)
+        self.otherSnakes = [Snake(self, [5,2], 0)]
+
+    def snakeCollision(self, snake):
+        coordinate = snake.chain[0]
+
+        # Check if collision with ownSnake
+
+        for i in range(len(self.ownSnake.chain)):
+            if i != 0 or snake != self.ownSnake:
+                if self.ownSnake.chain[i][0] == coordinate[0] and self.ownSnake.chain[i][1] == coordinate[1]:
+                    return self.ownSnake
+
+        # Check collision with one of the other snakes
+
+        for otherSnake in self.otherSnakes:
+            for i in range(len(otherSnake.chain)):
+                if i != 0 or snake != otherSnake:
+                    if otherSnake.chain[i][0] == coordinate[0] and otherSnake.chain[i][1] == coordinate[1]:
+                        return otherSnake
+
+        return False
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -82,6 +129,17 @@ class Game:
             if event.key == pygame.K_s:
                 self.ownSnake.setDirection(1)
 
+
+            if event.key == pygame.K_j:
+                self.otherSnakes[0].setDirection(2)
+            if event.key == pygame.K_i:
+                self.otherSnakes[0].setDirection(3)
+            if event.key == pygame.K_l:
+                self.otherSnakes[0].setDirection(0)
+            if event.key == pygame.K_k:
+                self.otherSnakes[0].setDirection(1)
+
+
     def on_loop(self):
         self.deltaTicks = pygame.time.get_ticks() - self.ticksPassed
         self.ticksPassed = pygame.time.get_ticks()
@@ -95,7 +153,6 @@ class Game:
         self._display_surf.fill([0, 0, 0])
 
         self.ownSnake.on_render(self._display_surf)
-
         for i in self.otherSnakes:
             i.on_render(self._display_surf)
 
